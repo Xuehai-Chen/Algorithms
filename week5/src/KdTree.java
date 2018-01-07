8248ededba396b059d2a24c234b5a1bd51e425fa
Xuehai-Chen <13162576651@163.com>
2018-01-07 17:55:30 +0800

    specify import

--- week5/src/KdTree.java
+++ week5/src/KdTree.java
@@ -3,9 +3,6 @@
  */
 
 import edu.princeton.cs.algs4.*;
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdOut;
 
 public class KdTree {
     private Node root;
