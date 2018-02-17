e87d9ac2f32289f85758f6b88ebd1ebe524dada4
Xuehai-Chen <13162576651@163.com>
2018-02-17 17:06:43 +0800

    get 41% for missing a lot of valid word.

--- Boggle/src/BoggleSolver.java
+++ Boggle/src/BoggleSolver.java
@@ -1,22 +1,23 @@
 import edu.princeton.cs.algs4.Graph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.TrieST;
+import edu.princeton.cs.algs4.TST;
 
 import java.util.HashSet;
 
 public class BoggleSolver {
 
     private HashSet<String> set = new HashSet<>();
-    private TrieST<Integer> dic;
+    private TST<Integer> dic;
     private Graph graph;
     private BoggleBoard board;
+    private int cols;
+    private int rows;
 
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
-        this.setGraph();
-        this.dic = new TrieST<>();
+        this.dic = new TST<>();
         for (int i = 0; i < dictionary.length; i++) {
             this.dic.put(dictionary[i], i);
         }
@@ -27,9 +28,7 @@ public class BoggleSolver {
         In in = new In(args[0]);
         String[] dictionary = in.readAllStrings();
         BoggleSolver solver = new BoggleSolver(dictionary);
-        StdOut.println(solver.graph.toString());
         BoggleBoard board = new BoggleBoard(args[1]);
-        StdOut.println(board.toString());
         int score = 0;
         for (String word : solver.getAllValidWords(board)) {
             StdOut.println(word);
@@ -39,61 +38,61 @@ public class BoggleSolver {
     }
 
     private void setGraph() {
-        this.graph = new Graph(16);
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        this.graph = new Graph(rows * cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (i != 0) {
-                    this.graph.addEdge(i * 4 + j, (i - 1) * 4 + j);
+                    this.graph.addEdge(i * rows + j, (i - 1) * rows + j);
                 }
                 if (j != 0) {
-                    this.graph.addEdge(i * 4 + j, i * 4 + j - 1);
+                    this.graph.addEdge(i * rows + j, i * rows + j - 1);
                 }
                 if (i != 0 && j != 0) {
-                    this.graph.addEdge(i * 4 + j, (i - 1) * 4 + j - 1);
+                    this.graph.addEdge(i * rows + j, (i - 1) * rows + j - 1);
                 }
                 if (i != 0 && j != 3) {
-                    this.graph.addEdge(i * 4 + j, (i - 1) * 4 + j + 1);
+                    this.graph.addEdge(i * rows + j, (i - 1) * rows + j + 1);
                 }
             }
         }
+        //StdOut.println(this.graph);
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
         this.board = board;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                this.set.addAll(findWords(i, j));
+        this.cols = board.cols();
+        this.rows = board.rows();
+        setGraph();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                boolean[] marked = new boolean[rows * cols];
+                StringBuffer cur = new StringBuffer();
+                dfs(cur, rows * i + j, marked);
             }
         }
         return this.set;
     }
 
-    private HashSet<String> findWords(int i, int j) {
-        HashSet<String> res = new HashSet<>();
-        boolean[] marked = new boolean[16];
-        StringBuffer cur = new StringBuffer();
-        cur.append(board.getLetter(i, j));
-        return dfs(res, cur, 0, marked);
-    }
-
-    private HashSet<String> dfs(HashSet<String> res, StringBuffer cur, int v, boolean[] marked) {
+    private void dfs(StringBuffer cur, int v, boolean[] marked) {
         marked[v] = true;
+        cur.append(board.getLetter(v / rows, v % rows));
+        if (cur.length() > 2 && !this.dic.keysThatMatch(cur.toString()).iterator().hasNext()) {
+            //StdOut.println("this is not a prefix in the dict: " + cur.toString());
+            cur.deleteCharAt(cur.length() - 1);
+            marked[v] = false;
+            return;
+        }
         for (int w : this.graph.adj(v)) {
             if (!marked[w]) {
-                cur.append(board.getLetter(w / 4, w % 4));
-                if (this.dic.keysWithPrefix(cur.toString()) == null) {
-                    return null;
-                } else {
-                    StdOut.println(cur.toString());
-                    dfs(res, cur, w, marked);
-                    if (this.dic.contains(cur.toString()) && cur.length() > 2) {
-                        res.add(cur.toString());
-                    }
-                }
+                dfs(cur, w, marked);
             }
         }
-        return res;
+        if (this.dic.contains(cur.toString()) && cur.length() > 2) {
+            set.add(cur.toString());
+        }
+        cur.deleteCharAt(cur.length() - 1);
+        marked[v] = false;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
