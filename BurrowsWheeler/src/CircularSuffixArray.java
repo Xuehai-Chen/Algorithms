2a75f37b99691ac5e3c5a55f805b4e5bf0043fa6
Xuehai-Chen <13162576651@163.com>
2018-02-21 11:04:56 +0800

    api commit

--- BurrowsWheeler/src/CircularSuffixArray.java
+++ BurrowsWheeler/src/CircularSuffixArray.java
@@ -1,2 +1,24 @@
 public class CircularSuffixArray {
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+
+    // length of s
+    public int length() {
+        int res = 0;
+        return res;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        int res = 0;
+        return res;
+    }
+
 }
