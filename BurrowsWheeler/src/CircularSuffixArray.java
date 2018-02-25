066851af37703b9c769d49f4d359a1ad1629e89c
Xuehai-Chen <13162576651@163.com>
2018-02-25 16:54:40 +0800

    optimize circular suffix array

--- BurrowsWheeler/src/CircularSuffixArray.java
+++ BurrowsWheeler/src/CircularSuffixArray.java
@@ -3,14 +3,13 @@ import edu.princeton.cs.algs4.In;
 public class CircularSuffixArray {
     private int[] index;
     private int N;
-    private String[] circularSuffixArray;
+    private int[][] circularSuffixArray;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
         if (s == null) throw new IllegalArgumentException();
         this.N = s.length();
         index = new int[this.N];
-        circularSuffixArray = new String[this.N];
         setCircularSuffixArray(s);
         setSortedArray(circularSuffixArray, this.N);
     }
@@ -20,40 +19,43 @@ public class CircularSuffixArray {
         In in = new In(args[0]);
         CircularSuffixArray c = new CircularSuffixArray(in.readString());
         System.out.println("");
-
         for (int i = 0; i < c.index.length; i++) {
             System.out.println(c.index[i]);
         }
-        //System.out.println(c.length());
+        System.out.println(c.length());
 
     }
 
     private void setCircularSuffixArray(String s) {
-        String temp = new String(s);
+        int[] source = new int[N];
+        for(int i=0;i<N;i++) {
+            source[i] = s.charAt(i);
+        }
+        circularSuffixArray = new int[N][N];
         for (int i = 0; i < circularSuffixArray.length; i++) {
-            circularSuffixArray[i] = temp;
-            temp = temp.substring(1) + temp.substring(0, 1);
-            System.out.println(circularSuffixArray[i]);
+            for(int j = i;j<N+i;j++) {
+                circularSuffixArray[i][j-i] = source[j % N];
+            }
         }
     }
 
-    private void setSortedArray(String[] a, int W) {
+    private void setSortedArray(int[][] a, int W) {
         int N = a.length;
         int R = 256;
-        String[] sortedArray = new String[N];
+        int[][] sortedArray = new int[N][N];
         int[] temp = new int[this.N];
         for (int d = W - 1; d >= 0; d--) { // Sort by key-indexed counting on dth char.
             int[] count = new int[R + 1]; // Compute frequency counts.
             for (int i = 0; i < N; i++)
-                count[a[i].charAt(d) + 1]++;
+                count[a[i][d] + 1]++;
             for (int r = 0; r < R; r++) // Transform counts to indices.
                 count[r + 1] += count[r];
             for (int i = 0; i < N; i++) {
-                sortedArray[count[a[i].charAt(d)]++] = a[i];
+                sortedArray[count[a[i][d]]++] = a[i];
                 if (d == W - 1) {
                     index[i] = i;
                 }
-                temp[count[a[i].charAt(d)] - 1] = index[i];
+                temp[count[a[i][d]] - 1] = index[i];
             }
             for (int i = 0; i < N; i++) {
                 a[i] = sortedArray[i];
