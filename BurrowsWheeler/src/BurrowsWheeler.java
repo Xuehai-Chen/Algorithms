d7ca3564ceb001a4402143418b49faee0895d5a6
Xuehai-Chen <13162576651@163.com>
2018-03-02 00:57:11 +0800

    98/100

--- BurrowsWheeler/src/BurrowsWheeler.java
+++ BurrowsWheeler/src/BurrowsWheeler.java
@@ -1,22 +1,28 @@
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
+import java.util.ArrayList;
+
 
 public class BurrowsWheeler {
     private static String s;
-    private static CircularSuffixArray initialize() {
+    private static CircularSuffixArray c;
+
+    private static void initialize() {
         StringBuilder sb = new StringBuilder();
         while (!BinaryStdIn.isEmpty()) {
             char newchar = BinaryStdIn.readChar(8);
             sb.append(newchar);
         }
         s = sb.toString();
-        return new CircularSuffixArray(s);
+        c = new CircularSuffixArray(s);
     }
 
     // apply Burrows-Wheeler transform, reading from standard input and writing to standard output
     public static void transform() {
-        CircularSuffixArray c = initialize();
+
+        initialize();
+
         int N = c.length();
         for (int i = 0; i < N; i++) {
             if (c.index(i) == 0) {
@@ -32,7 +38,40 @@ public class BurrowsWheeler {
 
     // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
     public static void inverseTransform() {
-        initialize();
+
+        ArrayList<Character> list = new ArrayList<>();
+        int first;
+        int next[];
+        int R = 256;
+
+        if (BinaryStdIn.isEmpty()) throw new IllegalArgumentException();
+        first = BinaryStdIn.readInt();
+
+        int n = 0;
+        while (!BinaryStdIn.isEmpty()) {
+            char newchar = BinaryStdIn.readChar(8);
+            list.add(newchar);
+            n++;
+        }
+
+        next = new int[n];
+        int[] count = new int[R + 1];
+        for (int i = 0; i < n; i++) {
+            count[list.get(i) + 1]++;
+        }
+        for (int i = 0; i < R; i++) {
+            count[i + 1] += count[i];
+        }
+        for (int i = 0; i < n; i++) {
+            next[count[list.get(i)]++] = i;
+        }
+        int offset = first;
+        for (int i = 0; i < n; i++) {
+            BinaryStdOut.write(list.get(next[offset]));
+            offset = next[offset];
+        }
+
+        BinaryStdOut.close();
     }
 
     // if args[0] is '-', apply Burrows-Wheeler transform
